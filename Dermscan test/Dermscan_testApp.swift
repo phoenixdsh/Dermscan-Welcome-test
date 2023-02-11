61e270fb76e876cc673607fdf2db93a1a88c7d03
phoenixdsh <ding200403@berkeley.edu>
2023-02-11 15:34:52 -0800

    Initial Commit

---
+++ Dermscan test/Dermscan_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dermscan_testApp.swift
+//  Dermscan test
+//
+//  Created by Phoenix Ding on 2023/2/11.
+//
+
+import SwiftUI
+
+@main
+struct Dermscan_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
